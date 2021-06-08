32324141e878021b3e4fb645e9656a10dced941d
SorataBaka <82799882+SorataBaka@users.noreply.github.com>
2021-06-08 22:24:27 +0700

    added slowmode command

---
+++ commands/moderation/slowmode.js
@@ -0,0 +1,30 @@
+const commando = require('discord.js-commando')
+const { MessageEmbed } = require('discord.js')
+module.exports = class ModerationCommand extends commando.Command {
+    constructor(client){
+        super(client, {
+            name: 'slowmode',
+            description: 'Sets slowmode for channel',
+            group:'moderations',
+            memberName: 'slowmode',
+            aliases: ['slowmode', 'slow', 'sm', 'slw'],
+            userPermissions: ['BAN_MEMBERS', 'KICK_MEMBERS', 'ADMINISTRATOR'],
+            argsType: 'multiple',
+        })
+    }
+    async run(message, args){
+        const channelID = message.channel.id
+        if(args.length == 0){
+            message.channel.guild.channels.cache.get(channelID).setRateLimitPerUser(0)
+        }else if(args[0] == 'none'||args[0] == 'off'){
+            message.channel.guild.channels.cache.get(channelID).setRateLimitPerUser(0)
+        }else if(isNaN(args[0])) {
+            message.reply("Please provide me a number!")
+        }else{
+            const slowmodeTime = args[0]
+            const slowmodeReason = args.slice(1).join(' ')
+            message.channel.guild.channels.cache.get(channelID).setRateLimitPerUser(slowmodeTime, slowmodeReason)
+        }
+    }   
+}
+
