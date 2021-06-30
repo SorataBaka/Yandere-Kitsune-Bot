f13f02d3382f0cf339a1650b72da936958e4ed74
SorataBaka <82799882+SorataBaka@users.noreply.github.com>
2021-07-01 04:38:55 +0700

    added role delete event

--- events/roleDelete.js
+++ events/roleDelete.js
@@ -3,10 +3,31 @@ module.exports = async(client, role) =>{
   const roleid = role.id
   const offerQuery = await tradeRequest.find({tradeRoleOffer: roleid})
   if(offerQuery.length > 0){
-    await tradeRequest.find({tradeRoleOffer: roleid}).deleteMany()
+    for(var i = 0; i < offerQuery.length; i++){
+      const authordata = offerQuery[i].tradeAuthor
+      const guildid = offerQuery[i].guildID
+      const tradetoken = offerQuery[i].tradeID
+      await client.guild.get(guildid).then(async (data, error) =>{
+        if(data){
+          guild.members.cache.get(authordata).send("You trade request with the ID : " + tradetoken + " has been cancelled due to the role being deleted").catch()
+        }
+      })
+    }
+    await tradeRequest.find({tradeRoleOffer: roleid}).deleteMany().catch()
   }
   const requestQuery = await tradeRequest.find({tradeRoleRequest: roleid})
   if(requestQuery.length > 0){
-    await tradeRequest.find({tradeRoleRequest: roleid}).deleteMany()
+    if(request.length > 0){
+      for(var i = 0; i < requestQuery.length; i++){
+        const authordata = offerQuery[i].tradeAuthor
+        const guildid = offerQuery[i].guildID
+        const tradetoken = offerQuery[i].tradeID
+        await client.guild.get(guildid).then(async (data, error) =>{
+          if(data){
+            guild.members.cache.get(authordata).send("You trade request with the ID : " + tradetoken + " has been cancelled due to the role being deleted").catch()
+          }
+        })
+      }
+      await tradeRequest.find({tradeRoleOffer: roleid}).deleteMany().catch()
   }
 }
