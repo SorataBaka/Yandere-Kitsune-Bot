b5259b42d36e6cad4ea7961621fe478b4bb69d1b
SorataBaka <82799882+SorataBaka@users.noreply.github.com>
2021-07-19 06:09:17 +0700

    added image generation to role creation

--- commands/booster - trading/createcard.js
+++ commands/booster - trading/createcard.js
@@ -12,6 +12,9 @@ const templates = [
   "../../assets/templates/5.png",
   "../../assets/templates/6.png",
 ]
+
+const ImageKit = require("imagekit")
+const Canvas = require("canvas")
 module.exports = class TradingCommand extends commando.Command{
   constructor(client){
     super(client,{
@@ -60,7 +63,7 @@ module.exports = class TradingCommand extends commando.Command{
         .setTitle("Please give us a title for your role and card!")
         .setDescription("This will be the title thats displayed in your role and card! Choose wisely because you won't be able to change this later~")
         .setFooter("Type 'Cancel' to cancel card and role creation")
-      const sent = await message.channel.send(nameEmbed)
+      const sent = await message.reply(nameEmbed)
       if(!sent) {
         return message.reply("I'm sorry, we've encountered an issue. Please try again later!")
       }
@@ -85,7 +88,7 @@ module.exports = class TradingCommand extends commando.Command{
         .setDescription("This will be the color thats displayed in your role and card! Choose wisely because you won't be able to change this later~ Example : #FF9693 ")
         .addField("Color reference", "https://htmlcolorcodes.com/")
         .setFooter("Type 'Cancel' to cancel card and role creation")
-    const sent = await message.channel.send(colorEmbed)
+    const sent = await message.reply(colorEmbed)
     if(!sent) {
       return message.reply("I'm sorry, we've encountered an issue. Please try again later!")
     }
@@ -108,7 +111,7 @@ module.exports = class TradingCommand extends commando.Command{
         .setDescription("This will be the image thats displayed in your card! Choose wisely because you won't be able to change this later~")
         .addField("WARNING", "Please only use PNG or JPG formatted card. Do not delete your picture after you have sent it as it will cause an error.")
         .setFooter("Type 'Cancel' to cancel card and role creation")
-      const sent = await message.channel.send(imageEmbed)
+      const sent = await message.reply(imageEmbed)
       if(!sent) {
         return message.reply("I'm sorry, we've encountered an issue. Please try again later!")
       }
@@ -125,20 +128,51 @@ module.exports = class TradingCommand extends commando.Command{
         if(!roleMessage.first().content){
           const mediaurl = roleMessage.first().attachments.first().url
           if(mediaurl.endsWith('.png') ||( mediaurl.endsWith(".jpg") || mediaurl.endsWith(".jpeg"))){
-            image = mediaurl
+            var imageurl = mediaurl
+            var imagekit = new ImageKit({
+              publicKey : "public_0J3v7HA2CfELfEzLdQOwu7VV4MY=",
+              privateKey : "private_3maKoCEAgQw6nt+lC1wz1UxOu9Q=",
+              urlEndpoint : "https://ik.imagekit.io/yanderekitsune"
+            });
+            message.reply("I am uploading your image to our database. Please wait!")
+            await imagekit.upload({
+              file: imageurl,
+              fileName: "default.png"
+            }).then((response) =>{
+              image = response.url
+            }).catch(error =>{
+              console.log(error)
+              return message.reply("I am having some trouble uploading your image. Please try again later!")
+            })
           }else{
             message.reply("Your file is in an invalid format! Please only use PNG or JPG Images. Try again!")
             return imageCreation()
           }
           descCreation()
         }else if(roleMessage.first().content.endsWith('.png') || roleMessage.first().content.endsWith('.jpg')){
-          image = roleMessage.first().content
+          var imageurl = roleMessage.first().content
+          var imagekit = new ImageKit({
+            publicKey : "public_0J3v7HA2CfELfEzLdQOwu7VV4MY=",
+            privateKey : "private_3maKoCEAgQw6nt+lC1wz1UxOu9Q=",
+            urlEndpoint : "https://ik.imagekit.io/yanderekitsune"
+          });
+          message.reply("I am uploading your image to our database. Please wait!")
+          await imagekit.upload({
+            file: imageurl,
+            fileName: "default.png"
+          }).then((response) =>{
+            image = response.url
+          }).catch(error =>{
+            console.log(error)
+            return message.reply("I am having some trouble uploading your image. Please try again later!")
+          })
           descCreation()
         }else{
           message.reply("Your image is in an invalid format! Please only use PNG or JPG images. Try again!")
           return imageCreation()
         }
       }).catch(timeout => {
+        console.log(timeout)
         return message.reply("You have ran out of time! Please try again.")})
     }
 
@@ -147,7 +181,7 @@ module.exports = class TradingCommand extends commando.Command{
         .setTitle("Please give us the description for your role and card!")
         .setDescription("This will be the description thats displayed on your card! Choose wisely because you won't be able to change this later~")
         .setFooter("Type 'Cancel' to cancel card and role creation")
-      const sent = await message.channel.send(descEmbed)
+      const sent = await message.reply(descEmbed)
       if(!sent) {
         return message.reply("I'm sorry, we've encountered an issue. Please try again later!")
       }
@@ -173,7 +207,7 @@ module.exports = class TradingCommand extends commando.Command{
         .setDescription("We have an assortment of templates made by Kitsune. Simply type a number corresponding to the template! Beware that this is a one-time thing. You won't be able to change this later")
         .setImage("https://cdn.discordapp.com/attachments/851502836478115852/865665338220871712/brochure.png")
         .setFooter("Type 'Cancel' to cancel card and role creation")
-      const sent = await message.channel.send(templateEmbed)
+      const sent = await message.reply(templateEmbed)
       if(!sent) {
         return message.reply("I'm sorry, we've encountered an issue. Please try again later!")
       }
@@ -202,9 +236,42 @@ module.exports = class TradingCommand extends commando.Command{
         return message.reply("You have ran out of time! Please try again.")})
     }
     const confirmation = async() => {
+      message.reply("Please wait while we prepare for your role.")
+      //image generation
+      var responseurl;
+      const canvas = Canvas.createCanvas(1528,2375)
+      const ctx = canvas.getContext('2d')
+      const loadimage = await Canvas.loadImage(image)
+      const newwidth = 1454
+      const newheight = (1454/loadimage.width)*loadimage.height
+      const background = await Canvas.loadImage(`${__dirname}/${cardTemplate}`)
+      await ctx.drawImage(loadimage, 88 , 250 , newwidth , newheight)
+      await ctx.drawImage(background, 0 , 0 ,1528, 2375)
+      const buffer = canvas.toBuffer()
+      //cdn usage
+      var imagekit = new ImageKit({
+        publicKey : "public_0J3v7HA2CfELfEzLdQOwu7VV4MY=",
+        privateKey : "private_3maKoCEAgQw6nt+lC1wz1UxOu9Q=",
+        urlEndpoint : "https://ik.imagekit.io/yanderekitsune"
+      });
+      await imagekit.upload({
+        file: buffer,
+        fileName: "default.png"
+      }).then((response) =>{
+        responseurl = response.url
+      }).catch(error =>{
+        console.log(error)
+        return message.reply("I am having some trouble uploading your image. Please try again later!")
+      })
+      //send the confirmation embed
       const confirmationEmbed = new MessageEmbed()
-        .setTitle("Type 'Confirm' to complete the card creation")
-      message.channel.send(confirmationEmbed)
+        .setTitle("Please check the following information.")
+        .setDescription("This will be the final card that you receive. Are you sure you want to confirm?")
+        .setImage(responseurl)
+        .setFooter("Type Confirm to finish or Cancel to abort")
+        .setTimestamp()
+        .setColor(`#BDB5B5`)
+      message.reply({embed: confirmationEmbed})
       message.channel.awaitMessages(filter, {
         max: 1,
         time: 30000
@@ -219,6 +286,7 @@ module.exports = class TradingCommand extends commando.Command{
         }
       })
       .catch(error =>{
+        console.log(error)
         message.reply("You have timed out! Please try again.")
       })
     }
--- commands/booster - utilities/card.js
+++ commands/booster - utilities/card.js
@@ -1,7 +1,9 @@
 const commando = require('discord.js-commando')
 const claimedSchema = require('../../schema/childschema/claimedroleschema.js')
 const Canvas = require('canvas')
-const { MessageAttachment } = require('discord.js')
+const { MessageAttachment, MessageEmbed } = require('discord.js')
+const ImageKit = require('imagekit')
+const axios = require('axios')
 module.exports = class UtilityCommand extends commando.Command{
   constructor(client){
     super(client,{
@@ -15,26 +17,48 @@ module.exports = class UtilityCommand extends commando.Command{
   }
   async run(message, args){
     const { guild, client, author } = message
-    
+    message.reply("Please wait while we get your card!")
+    const { privatekey } = process.env
     const rolequery = await claimedSchema.find({userID: author.id, guildID: guild.id})
-    console.log(rolequery)
     if(rolequery.length == 0) return message.reply("I can't find this role!")
     const imageurl = rolequery[0].cardImageURL
     const template = rolequery[0].template
-
+    const description = rolequery[0].description
+    const name = rolequery[0].roleName
     const canvas = Canvas.createCanvas(1528,2375)
     const ctx = canvas.getContext('2d')
     const image = await Canvas.loadImage(imageurl)
-    console.log(image.width)
-    console.log(image.height)
     const newwidth = 1454
     const newheight = (1454/image.width)*image.height
     const background = await Canvas.loadImage(`${__dirname}/${template}`)
-    await ctx.drawImage(image, 88 , 125 , newwidth , newheight)
+    await ctx.drawImage(image, 88 , 250 , newwidth , newheight)
     await ctx.drawImage(background, 0 , 0 ,1528, 2375)
+    ctx.textAlign = `center`;
+    ctx.textBaseline = `top`;
+    await ctx.fillText(name, 764, 90 , 1325)
     const buffer = canvas.toBuffer();
-    const attachment = new MessageAttachment(buffer, 'image.png')
-    message.channel.send(attachment)
+    const picture = new MessageAttachment(buffer, 'image.png')
+    // var imagekit = new ImageKit({
+    //   publicKey : "public_0J3v7HA2CfELfEzLdQOwu7VV4MY=",
+    //   privateKey : "private_3maKoCEAgQw6nt+lC1wz1UxOu9Q=",
+    //   urlEndpoint : "https://ik.imagekit.io/yanderekitsune"
+    // });
+    // await imagekit.upload({
+    //   file: buffer,
+    //   fileName: "default.png"
+    // }).then((response) =>{
+    //   const embed = new MessageEmbed()
+    //     .setTitle(`${rolequery[0].roleName}`)
+    //     .setImage(response.url)
+    //     .setFooter("Here is your card!")
+    //     .setColor("#FF827F")
+    //     .setTimestamp()
+    //   message.reply(embed)
+    // }).catch(error =>{
+    //   console.log(error)
+    // })
+
+    message.reply(picture)
     
   }
 }
