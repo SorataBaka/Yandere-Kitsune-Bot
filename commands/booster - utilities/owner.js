c62202985c131d6d158e7b07cd5c6e9184ae231c
SorataBaka <82799882+SorataBaka@users.noreply.github.com>
2021-07-19 08:50:08 +0700

    added role owner checking function

---
+++ commands/booster - utilities/owner.js
@@ -0,0 +1,43 @@
+const commando = require('discord.js-commando')
+const staffPing = require('../../schema/childschema/guilddata.js')
+const claimedSchema = require('../../schema/childschema/claimedroleschema.js')
+const { MessageEmbed } = require('discord.js')
+module.exports = class TradingCommand extends commando.Command{
+  constructor(client){
+    super(client,{
+      name: 'owner',
+      description: 'Checks the custom role owner',
+      group: 'trading',
+      memberName: 'owner',
+      argsType: 'multiple',
+      guildOnly: true
+    })
+  }
+  async run(message, args){
+    const { client, guild, author } = message
+    const guildid = guild.id
+    const authorid = author.id
+    if(!args) return message.reply("Please give me a role name or role ID!!")
+    const roleinfo = args.join(' ')
+    if(isNaN(roleinfo)){
+      const query = await claimedSchema.find({guildID: guildid, roleName: roleinfo})
+      if(query.length == 0) return message.reply("I can't find this role!")
+      const rolename = query[0].roleName
+      const roleowner = query[0].userID
+      const ownername = guild.members.cache.get(roleowner).user.tag
+      const embed = new MessageEmbed()
+        .setAuthor(`The owner for role ${rolename} is ${ownername}`)
+      message.channel.send(embed)
+    }else{
+      const query = await claimedSchema.find({guildID: guildid, roleID: roleinfo})
+      if(query.length == 0) return message.reply("I can't find this role!")
+      const rolename = query[0].roleName
+      const roleowner = query[0].userID
+      const ownername = guild.members.cache.get(roleowner).user.tag
+      const embed = new MessageEmbed()
+        .setAuthor(`The owner for role ${rolename} is ${ownername}`)
+      message.channel.send(embed)
+    }
+    
+  }
+}
