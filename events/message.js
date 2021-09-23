dfe334cc166676a79b63b27ecf35947dfcc36f95
Christian Harjuno <harjuno.tian@gmail.com>
2021-09-24 06:32:49 +0700

    Changed message reaction system

--- events/message.js
+++ events/message.js
@@ -1,16 +1,22 @@
 const fs = require('fs');
-const imageArray = JSON.parse(fs.readFileSync("./assets/messagePics/images.json"))
-const messagesArray = JSON.parse(fs.readFileSync("./assets/messagePics/texts.json"))
 const { MessageAttachment, MessageEmbed } = require("discord.js")
-
+const axios = require('axios')
 module.exports = async(client, message)=>{
    const content = message.content
+   const messageCall = await axios.get("http://40.65.136.244:10000/generatewords")
+   if(!messageCall) return
+   const newMessage = messageCall.data.Word
+   
+   const imageCall = await axios.get("http://40.65.136.244:10000/image")
+   if(!imageCall) return
+   const imageArray = imageCall.data.imageList
+   const randomPick = Math.floor(Math.random() * imageArray.length)
+   const imageName = imageCall.data.imageList[randomPick]
+
    if(content.toLowerCase() == "kitsu sayang"){
-        const imageRandomNumber = Math.floor(Math.random() * imageArray.length)
-        const messageRandomNumber = Math.floor(Math.random() * messagesArray.length)
         const embed = new MessageEmbed()
-            .setTitle(messagesArray[messageRandomNumber] + " - kitsu")
-            .setImage(imageArray[imageRandomNumber])
+            .setTitle(newMessage + " - kitsu")
+            .setImage(`http://40.65.136.244:10000/image/${imageName}`)
             .setColor("#FF7A7A")
             .setTimestamp()
         return message.channel.send(embed)
